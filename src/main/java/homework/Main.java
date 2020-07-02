0cb52b5f4790f26f461242538fb8d45930a01e94
Максим Матуляк <maksymmatuliak@gmail.com>
2020-07-02 20:35:26 +0300

    deleted magic numbers

--- src/main/java/homework/Main.java
+++ src/main/java/homework/Main.java
@@ -8,19 +8,23 @@ import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
 
 public class Main {
+    private static final int numbersOfTreads = 2;
+    private static final int firstPart = 1;
+    private static int secondPart = 2;
+
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         RandomNumberList numbers = new RandomNumberList();
         ThreadCounter threadCounter =
-                new ThreadCounter(numbers, 1, 2);
+                new ThreadCounter(numbers, firstPart, numbersOfTreads);
         ThreadCounter secondThread =
-                new ThreadCounter(numbers, 2, 2);
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
+                new ThreadCounter(numbers, secondPart, numbersOfTreads);
+        ExecutorService executorService = Executors.newFixedThreadPool(numbersOfTreads);
         List<Future<Integer>> futuresFromExecurotService =
                 executorService.invokeAll(List.of(threadCounter, secondThread));
         System.out.println("Sum is " + (futuresFromExecurotService.get(0).get()
                 + futuresFromExecurotService.get(1).get()));
         System.out.println("----------------------------------------------------------");
-        ForkJoinPool forkJoinPool = new ForkJoinPool(2);
+        ForkJoinPool forkJoinPool = new ForkJoinPool(numbersOfTreads);
         List<Future<Integer>> futuresFromForkJoin =
                 forkJoinPool.invokeAll(List.of(threadCounter, secondThread));
         System.out.println("Sum is " + (futuresFromForkJoin.get(0).get()
